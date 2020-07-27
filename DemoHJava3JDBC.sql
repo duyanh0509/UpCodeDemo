9b11528eb7cb8d985a49a2f93c1424af07ee2966
duyanh0509 <62171446+duyanh0509@users.noreply.github.com>
2020-07-27 09:00:40 +0700

    Add files via upload

---
+++ DemoHJava3JDBC.sql
@@ -0,0 +1,21 @@
+
+use ManagementNhanVien
+go
+
+select* from NhanVien
+
+delete from san_pham where id='42' 
+
+select* from san_pham
+
+ update san_pham set ten='Lambobo1' where ten like 'LamBO'
+
+
+
+
+ select*from SANPHAM
+ delete from SANPHAM where MASANPHAM=001
+ insert into SANPHAM values('002','Iphone9','5','12','5','2020-07-09','New','IOS')
+
+
+
